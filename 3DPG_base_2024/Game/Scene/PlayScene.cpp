8e335a2fcd42aa763fa69fd4eb955b17cbfababd
aoten0427 <aoi.kage0427@gmail.com>
2025-04-28 10:45:31 +0900

    no message

--- 3DPG_base_2024/Game/Scene/PlayScene.cpp
+++ 3DPG_base_2024/Game/Scene/PlayScene.cpp
@@ -71,6 +71,34 @@ void PlayScene::Initialize(CommonResources* resources)
 
 	// モデルを読み込む
 	m_model = DirectX::Model::CreateFromCMO(device, L"Resources/Models/dice.cmo", *fx);
+
+	// ベーシックエフェクトを作成する
+	m_basicEffect = std::make_unique<BasicEffect>(device);
+	m_basicEffect->SetVertexColorEnabled(true);
+	m_basicEffect->SetLightingEnabled(false);
+	m_basicEffect->SetTextureEnabled(false);
+
+	// 入力レイアウトを作成する
+	DX::ThrowIfFailed(
+		CreateInputLayoutFromEffect<VertexPositionColor>(
+			device,
+			m_basicEffect.get(),
+			m_inputLayout.ReleaseAndGetAddressOf()
+		)
+	);
+
+	// マルチスレッドレンダリングの初期化
+	if (m_useMultithreadedRendering)
+	{
+		for (int i = 0; i < THREAD_COUNT; i++)
+		{
+			m_threadDatas.emplace_back();
+			// 遅延コンテキストの作成
+			resources->GetDeviceResources()->GetD3DDevice()->CreateDeferredContext(0, &m_threadDatas[i].deferredContext);
+			m_threadDatas[i].commandList = nullptr;
+			m_threadDatas[i].working = false;
+		}
+	}
 }
 
 //---------------------------------------------------------
@@ -101,18 +129,93 @@ void PlayScene::Render()
 	//第二引数　回転角度
 	//Y軸で90度回転
 
-	
 	Matrix mat = Matrix::Identity;
 
-	// モデルを描画する
-	m_model->Draw(context, *states, mat, view, m_projection, false, [&]() {
-			
-		});
+	//// モデルを描画する
+	//m_model->Draw(context, *states, mat, view, m_projection, false, [&]() {
+	//		
+	//	});
 
 
 	// デバッグ情報を「DebugString」で表示する
 	auto debugString = m_commonResources->GetDebugString();
 	debugString->AddString("Play Scene");
+
+
+    if (m_useMultithreadedRendering)
+    {
+        // モデルの総数を取得
+		size_t modelCount = 10;
+        size_t modelsPerThread = modelCount / THREAD_COUNT;
+
+        // レンダリングタスクを分割
+        for (int i = 0; i < THREAD_COUNT; i++)
+        {
+            m_threadDatas[i].commands.clear();
+
+            // このスレッドで処理するモデルの範囲を決定
+            size_t startIndex = i * modelsPerThread;
+            size_t endIndex = (i == THREAD_COUNT - 1) ? modelCount : (i + 1) * modelsPerThread;
+
+            // 各モデルの描画コマンドを追加
+            for (size_t j = startIndex; j < endIndex; j++)
+            {
+                // ワールド行列などの計算
+				DirectX::SimpleMath::Matrix world = Matrix::Identity;
+
+                /*m_model->Draw(context, *states, world, view, m_projection);*/
+                //// 描画コマンドをラムダ式でキャプチャ
+                m_threadDatas[i].commands.push_back(RenderCommand{
+                    [&](ID3D11DeviceContext* ctx) {
+                        // モデルの描画
+                        m_model->Draw(ctx, *states, world,view, m_projection);
+                    }
+                    });
+            }
+
+            // スレッドを開始
+            m_threadDatas[i].working = true;
+            if (m_threadDatas[i].thread)
+            {
+                m_threadDatas[i].thread = nullptr;  // 前のスレッドをクリア
+            }
+            m_threadDatas[i].thread = std::make_unique<std::thread>(&PlayScene::RenderThread, this, i);
+        }
+
+        // 全スレッドの完了を待つ
+        for (int i = 0; i < THREAD_COUNT; i++)
+        {
+            if (m_threadDatas[i].thread && m_threadDatas[i].thread->joinable())
+            {
+                m_threadDatas[i].thread->join();
+            }
+        }
+
+        // コマンドリストを実行
+        for (int i = 0; i < THREAD_COUNT; i++)
+        {
+            if (m_threadDatas[i].commandList)
+            {
+                context->ExecuteCommandList(m_threadDatas[i].commandList, FALSE);
+                m_threadDatas[i].commandList->Release();
+                m_threadDatas[i].commandList = nullptr;
+            }
+        }
+    }
+    else
+    {
+        //// シングルスレッドレンダリング（既存の処理）
+        //for (size_t i = 0; i < m_models.size(); i++)
+        //{
+        //    DirectX::SimpleMath::Matrix world = CalculateWorldMatrix(i);
+
+        //    context->IASetInputLayout(m_states->inputLayout.Get());
+        //    m_effect->SetWorld(world);
+        //    m_effect->Apply(context);
+
+        //    m_models[i]->Draw(context, *m_states, world, m_view, m_projection);
+        //}
+    }
 }
 
 //---------------------------------------------------------
@@ -136,4 +239,20 @@ IScene::SceneID PlayScene::GetNextSceneID() const
 
 	// シーン変更がない場合
 	return IScene::SceneID::NONE;
+}
+
+void PlayScene::RenderThread(int threadIndex)
+{
+    ThreadData& threadData = m_threadDatas[threadIndex];
+    ID3D11DeviceContext* context = threadData.deferredContext;
+
+    // このスレッドに割り当てられたコマンドを実行
+    for (const auto& command : threadData.commands)
+    {
+        command.renderFunc(context);
+    }
+
+    // コマンドリストを作成
+    threadData.deferredContext->FinishCommandList(FALSE, &threadData.commandList);
+    threadData.working = false;
 }
--- 3DPG_base_2024/Game/Scene/PlayScene.h
+++ 3DPG_base_2024/Game/Scene/PlayScene.h
@@ -4,6 +4,8 @@
 */
 #pragma once
 #include "IScene.h"
+#include <thread>
+#include <mutex>
 
 // 前方宣言
 class CommonResources;
@@ -35,7 +37,64 @@ private:
 	std::unique_ptr<DirectX::Model> m_model;
 
 	
+	// マルチスレッドレンダリング用の構造体
+	struct RenderCommand
+	{
+		std::function<void(ID3D11DeviceContext*)> renderFunc;
+	};
 
+    struct ThreadData
+    {
+        ID3D11DeviceContext* deferredContext;
+        ID3D11CommandList* commandList;
+        std::vector<RenderCommand> commands;
+        std::unique_ptr<std::thread> thread;  // threadをユニークポインタに変更
+        std::mutex mutex;
+        std::atomic<bool> working;
+
+        // デフォルトコンストラクタを明示的に定義
+        ThreadData() : deferredContext(nullptr), commandList(nullptr), working(false) {}
+
+        // ムーブコンストラクタを定義
+        ThreadData(ThreadData&& other) noexcept
+            : deferredContext(other.deferredContext)
+            , commandList(other.commandList)
+            , commands(std::move(other.commands))
+            , thread(std::move(other.thread))
+            , working(other.working.load())
+        {
+            other.deferredContext = nullptr;
+            other.commandList = nullptr;
+        }
+
+        // コピーを禁止
+        ThreadData(const ThreadData&) = delete;
+        ThreadData& operator=(const ThreadData&) = delete;
+
+        // ムーブ代入演算子を定義
+        ThreadData& operator=(ThreadData&& other) noexcept
+        {
+            if (this != &other)
+            {
+                deferredContext = other.deferredContext;
+                commandList = other.commandList;
+                commands = std::move(other.commands);
+                thread = std::move(other.thread);
+                working = other.working.load();
+
+                other.deferredContext = nullptr;
+                other.commandList = nullptr;
+            }
+            return *this;
+        }
+    };
+
+	// マルチスレッドレンダリング用のメンバー変数
+	std::vector<ThreadData> m_threadDatas;
+	bool m_useMultithreadedRendering;
+	static const int THREAD_COUNT = 4; // スレッド数
+    std::unique_ptr<DirectX::BasicEffect> m_basicEffect;
+    Microsoft::WRL::ComPtr<ID3D11InputLayout> m_inputLayout;
 public:
 	PlayScene();
 	~PlayScene() override;
@@ -46,4 +105,6 @@ public:
 	void Finalize() override;
 
 	SceneID GetNextSceneID() const;
+
+    void RenderThread(int threadIndex);
 };
